e794b6c0c15fb9acb92099cb8121c7d3c52e64d1
MH <mhagajju@gmail.com>
2023-06-10 09:02:29 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           We Are EduCareer and going to host on IPFS 
         </p>
         <a
           className="App-link"
